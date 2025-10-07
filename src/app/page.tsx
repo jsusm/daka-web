945939f4ae59b6d8efb581f9b7cf3f216216e78d
jsusm <jesusjaviermarcanomora.2005@gmail.com>
2025-10-07 16:26:25 -0400

    Add SectionHowToBuy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,13 @@
-import { LucideArrowRight, LucideLampDesk, LucideMonitorSmartphone, LucideRefrigerator, LucideSearch, LucideShoppingCart, LucideTvMinimal, Section, UserIcon } from 'lucide-react'
 import Footer from "@/components/layout/Footer";
-import SectionFeatures from "./sections/SectionFeatures";
 import SectionCategories from "./sections/SectionCategories";
 import HeroSection from "./sections/SectionHero";
 import Header from "@/components/layout/Header";
 import SectionPartnerBrands from "./sections/SectionParnerBrands";
 import SectionDakaBanners from "./sections/SectionDakaBanners";
 import ProductsSectionPromo from "./sections/SectionProductsPromo";
-import SectionCashea from "./sections/SectionCashea";
 import SectionGanaConDaka from './sections/SectionGanaConDaka';
 import SectionSucursales from './sections/SectionSucursales';
+import { SectionHowToBuy } from "./sections/SectionHowToBuy";
 
 
 const products = [
@@ -105,6 +103,7 @@ export default function Home() {
       <main className="font-sans">
         <HeroSection />
         <SectionCategories />
+        <SectionHowToBuy />
         <ProductsSectionPromo products={products} title="Productos Pensados para ti" />
         <SectionPartnerBrands />
         <SectionGanaConDaka />
---
+++ src/app/sections/SectionHowToBuy.tsx
@@ -0,0 +1,50 @@
+import SectionLayout from "@/components/layout/SectionLayout";
+import { SectionTitle, SectionTitleLabel } from "@/components/layout/SectionTitle";
+import { Check, ShoppingCart, User } from "lucide-react";
+
+const steps = [
+  {
+    icon: <ShoppingCart size={32} className="text-blue-900" />,
+    title: "Selecciona tus productos",
+    subtitle: "Explora y elige lo que necesites",
+  },
+  {
+    icon: <User size={32} className="text-blue-900" />,
+    title: "Completa tus datos",
+    subtitle: "Llena el formulario para tu pedido",
+  },
+  {
+    icon: <Check size={32} className="text-blue-900" />,
+    title: "Finaliza tu compra",
+    subtitle: "Un asesor de ventas te contactará",
+  },
+];
+
+export const SectionHowToBuy = () => {
+  return (
+    <SectionLayout>
+      <SectionTitle >
+        <div>
+          <SectionTitleLabel> Cómo comprar? </SectionTitleLabel>
+          <p className="text-stone-600">Sigue estos simples pasos para completar tu compra atravez de la web de Daka.</p>
+        </div>
+      </SectionTitle>
+      <div className="grid grid-cols-1 md:grid-cols-3 mt-12">
+        {steps.map((step, index) => (
+          <div key={index} className="flex flex-col md:flex-row md:items-center">
+            <div className="flex items-center gap-4 p-2">
+              <div className="bg-blue-100 text-white rounded-full p-3">
+                {step.icon}
+              </div>
+              <div>
+                <p className="font-semibold text-lg text-stone-800">{step.title}</p>
+                <p className="text-stone-600">{step.subtitle}</p>
+              </div>
+            </div>
+            {index < steps.length - 1 && <div className=" md:w-auto md:h-16 mx-4 border-b my-4 md:border-b-0 md:border-l border-stone-300" />}
+          </div>
+        ))}
+      </div>
+    </SectionLayout>
+  );
+};
